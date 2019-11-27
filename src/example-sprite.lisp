36e02b83bfa82e7adc07d51b6bf7463ce0350205
Michael Fiano <mail@michaelfiano.com>
2019-11-27 01:20:34 -0500

    Minor changes to sprite example

--- src/example-sprite.lisp
+++ src/example-sprite.lisp
@@ -22,22 +22,27 @@
   (background :draw-order 0)
   (ships :draw-order 1))
 
+(pyx:define-prefab planet (:template sprite)
+  :group/name 'background
+  :xform/scale 2
+  :sprite/name "planet11")
+
+(pyx:define-prefab ship ()
+  :group/name 'ships
+  :xform/scale 1.2
+  :xform/translate (v3:vec 0 -120 0)
+  (body (:template sprite)
+        :sprite/name "ship29")
+  (exhaust (:template animated-sprite)
+           :xform/translate (v3:vec 0 -145 0)
+           :xform/scale (v3:vec 1 0.65 1)
+           :sprite/name "exhaust01-01"
+           :sprite/frames 8
+           :animate/sequence 'sprite))
+
 (pyx:define-prefab sprite-scene ()
   (camera (:template camera/orthographic)
           :camera/clip-near 0
           :camera/clip-far 16)
-  (ship ()
-        :group/name 'ships
-        :xform/scale 1.2
-        :xform/translate (v3:vec 0 -120 0)
-        (body (:template sprite)
-              :sprite/name "ship29")
-        (exhaust (:template animated-sprite)
-                 :xform/translate (v3:vec 0 -142 0)
-                 :sprite/name "exhaust03-01"
-                 :sprite/frames 8
-                 :animate/sequence 'sprite))
-  (planet (:template sprite)
-          :group/name 'background
-          :xform/scale 2
-          :sprite/name "planet02"))
+  (planet (:template (planet)))
+  (ship (:template (ship))))

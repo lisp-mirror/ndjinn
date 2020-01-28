67aecbeef705de0e82a80582c01bf91b01d695de
Michael Fiano <mail@michaelfiano.com>
2020-01-28 06:12:45 -0500

    more work on fonts

--- examples/font/definitions.lisp
+++ examples/font/definitions.lisp
@@ -1,59 +1,41 @@
 (in-package #:pyx.examples)
 
-;;; helper functions
+(defun seconds->duration (seconds)
+  (u:mvlet* ((days d-rem (floor seconds 86400))
+             (hours h-rem (floor d-rem 3600))
+             (minutes m-rem (floor h-rem 60))
+             (seconds (floor m-rem)))
+    (format nil "~dd ~dh ~dm ~ds" days hours minutes seconds)))
 
-(defun print-time (time &optional (padding 12))
+(defun print-time (time)
   (u:mvlet ((div rem (truncate (truncate time (/ 100)) 100)))
-    (format nil "~v:d.~2,'0d" padding div rem)))
+    (format nil "~:d.~2,'0d" div rem)))
 
 (defun print-frame-rate (fps)
   (if (plusp fps)
       (values (print-time fps)
-              (print-time (/ 1000 fps) 3))
-      (values "              -" "     -")))
-
-(defun update-font-text ()
-  (u:mvlet ((fps avg avg/10s avg/30s avg/60s (pyx::get-fps)))
-    (multiple-value-call #'format nil "CPU: ~a~%~
-               GPU: ~a~%~%~
-               Running time: ~,2f s~%~
-               Frame #: ~:d~%~
-               FPS (current): ~a (~a ms)~%~
-               FPS (10s avg.): ~a (~a ms)~%~
-               FPS (30s avg.): ~a (~a ms)~%~
-               FPS (1m avg.): ~a (~a ms)~%~
-               FPS (overall): ~a (~a ms)"
-      (pyx:get-cpu)
-      (pyx:get-gpu-make/model)
-      (pyx:get-total-time)
-      (pyx:get-frame-count)
-      (print-frame-rate fps)
-      (print-frame-rate avg/10s)
-      (print-frame-rate avg/30s)
-      (print-frame-rate avg/60s)
-      (print-frame-rate avg))))
-
-(defun print-hardware-info ()
-  (format nil "CPU: ~a~%GPU: ~a"
-          (pyx:get-cpu)
-          (pyx:get-gpu-make/model)))
+              (/ 1000 fps))
+      (values "-" "-")))
 
 (defun print-fps-labels ()
   (format nil
-          "Counters~%Elapsed Time:~%Frames Drawn:~%~%~
-           Frame Rates~%Current:~%Last 10s:~%Last 30s:~%Last 1m:~%Overall:"))
+          "CPU: ~a~%GPU: ~a~%~%~
+           Counters~%Elapsed Time:~%Frames Drawn:~%~%~
+           Frame Rates~%Current:~%Last 10s:~%Last 30s:~%Last 1m:~%Overall:"
+          (pyx:get-cpu)
+          (pyx:get-gpu-make/model)))
 
 (defun print-fps-times ()
   (u:mvlet ((fps avg avg/10s avg/30s avg/1m (pyx:get-fps)))
     (multiple-value-call #'format nil
-      "~a s ~%~
-       ~15:d~%~%~%~
+      "~a~%~
+       ~:d~%~%~%~
        ~a / ~,3f ms~%~
        ~a / ~,3f ms~%~
        ~a / ~,3f ms~%~
        ~a / ~,3f ms~%~
        ~a / ~,3f ms~%"
-      (print-time (pyx:get-total-time))
+      (seconds->duration (pyx:get-total-time))
       (pyx:get-frame-count)
       (print-frame-rate fps)
       (print-frame-rate avg/10s)
@@ -77,36 +59,20 @@
 
 ;;; prefabs
 
-(pyx:define-prefab debug-info (:add (pyx:render pyx:font))
-  :xform/scale 1
-  :font/texture 'font
-  :font/geometry 'text
-  :font/text 'update-font-text
-  :font/position :top-left
-  :font/offset (v2:vec 10 -10)
-  :render/materials '(font))
-
 (pyx:define-prefab debug-info ()
-  ((hardware-info :add (pyx:render pyx:font))
-   :font/geometry 'text
-   :font/texture 'font
-   :font/text 'print-hardware-info
-   :font/position :top-left
-   :font/offset (v2:vec 0.5 -0.5)
-   :render/materials '(font))
   ((fps-labels :add (pyx:render pyx:font))
    :font/geometry 'text
    :font/texture 'font
    :font/text 'print-fps-labels
    :font/position :top-left
-   :font/offset (v2:vec 0.5 -3.5)
+   :font/offset (v2:vec 0.5 -0.5)
    :render/materials '(font))
   ((fps-times :add (pyx:render pyx:font))
    :font/geometry 'text
    :font/texture 'font
    :font/text 'print-fps-times
    :font/position :top-left
-   :font/offset (v2:vec 10 -4.5)
+   :font/offset (v2:vec 12 -4.5)
    :render/materials '(font)))
 
 ;;; geometry

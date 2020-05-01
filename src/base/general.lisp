3f375337e5d2b43454c45a602dd7c2c8d599287d
Michael Fiano <mail@michaelfiano.com>
2020-05-01 14:14:17 -0400

    Minor change

--- src/base/general.lisp
+++ src/base/general.lisp
@@ -1,11 +1,7 @@
 (in-package #:pyx)
 
 (defmacro make-nested-dict (test-func &rest keys)
-  `(u:dict ,test-func
-           ,@(mapcan
-              (lambda (x)
-                `(,x (u:dict ,test-func)))
-              keys)))
+  `(u:dict ,test-func ,@(mapcan (lambda (x) `(,x (u:dict ,test-func))) keys)))
 
 (defun initialize-rng ()
   (setf *random-state* (make-random-state t)))

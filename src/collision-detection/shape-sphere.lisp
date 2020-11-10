064077bdb6cdb723d1b302bedebf2dc2ab43bd25
Michael Fiano <mail@michaelfiano.com>
2020-11-09 21:24:20 -0500

    Revert "structify collider shapes"
    This reverts commit 062acf801040096444b4724eb93c23603721cf71.

--- src/collision-detection/shape-sphere.lisp
+++ src/collision-detection/shape-sphere.lisp
@@ -1,15 +1,11 @@
 (in-package #:net.mfiano.lisp.pyx)
 
-(defstruct (collider-shape/sphere
-            (:constructor %make-collider-shape/sphere)
-            (:include collider-shape)
-            (:predicate nil)
-            (:copier nil))
-  (radius 1f0 :type single-float))
+(defclass collider-shape/sphere (collider-shape)
+  ((%radius :reader radius
+            :initarg :radius
+            :initform 1)))
 
-(defun make-collider-shape/sphere (&rest args)
-  (let ((shape (apply #'%make-collider-shape/sphere args)))
-    (v3:with-components ((s (get-scale (collider-shape-entity shape)
-                                       :space :world)))
-      (setf (collider-shape/sphere-radius shape) (max sx sy sz))
-      shape)))
+(defmethod initialize-instance :after ((instance collider-shape/sphere) &key)
+  (with-slots (%entity %radius) instance
+    (v3:with-components ((s (get-scale %entity :space :world)))
+      (setf %radius (max sx sy sz)))))
--- src/collision-detection/shape.lisp
+++ src/collision-detection/shape.lisp
@@ -1,21 +1,20 @@
 (in-package #:net.mfiano.lisp.pyx)
 
-(defstruct (collider-shape
-            (:constructor nil)
-            (:predicate nil)
-            (:copier nil))
-  (type nil :type symbol)
-  entity
-  (center (v3:vec) :type v3:vec)
-  (update-func (constantly nil) :type function))
+(defclass collider-shape ()
+  ((%type :reader shape-type
+          :initarg :type)
+   (%entity :reader entity
+            :initarg :entity)
+   (%center :reader center
+            :initarg :center
+            :initform (v3:vec))))
 
 (defun make-collider-shape (entity shape-spec)
   (destructuring-bind (type . args) (u:ensure-list shape-spec)
-    (let ((constructor (ecase type
-                         (sphere #'make-collider-shape/sphere)
-                         (box #'make-collider-shape/box))))
-      (apply constructor :type type :entity entity args))))
+    (let ((class (u:format-symbol :net.mfiano.lisp.pyx
+                                  "COLLIDER-SHAPE/~a"
+                                  type)))
+      (apply #'make-instance class :type type :entity entity args))))
 
-(defun update-collider-shape (collider)
-  (let ((shape (collider/shape collider)))
-    (funcall (collider-shape-update-func shape) shape)))
+(defgeneric update-collider-shape (shape)
+  (:method (shape)))
